5836250b95bc3646daeb9c2023d93360461e1822
Jorge Moreno <jjmorenoaudio@gmail.com>
2024-10-31 17:19:07 +0000

    my awesome random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let cardNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let cardSuit = ["hearts", "diamonds", "clubs", "spades"];
+
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomCardSuit = Math.floor(Math.random() * cardSuit.length);
+  document.querySelector("#card").innerHTML = `${cardNumber[randomCardNumber]}`;
+  // document.querySelectorAll(".suit");
+
+  const suits = document.querySelectorAll(".suit");
+  for (let i = 0; i < suits.length; i++) {
+    suits[i].classList.add(cardSuit[randomCardSuit]);
+  }
 };
